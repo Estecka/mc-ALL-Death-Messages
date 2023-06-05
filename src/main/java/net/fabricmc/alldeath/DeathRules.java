03a97bec177217d9332c46d46b8e9843eeadd81a
Estecka <estecka@galacsys.com>
2023-06-05 09:38:21 +0200

    Working gamerules

---
+++ src/main/java/net/fabricmc/alldeath/DeathRules.java
@@ -0,0 +1,28 @@
+package net.fabricmc.alldeath;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.CustomGameRuleCategory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.GameRules.Category;
+
+
+public class DeathRules implements ModInitializer {
+	public static final GameRules.Key<GameRules.BooleanRule> NAMED_DEATH = BooleanRule("Named", true);
+	public static final GameRules.Key<GameRules.BooleanRule> OTHER_DEATH = BooleanRule("Other", false);
+
+	// public static final CustomGameRuleCategory CATEGORY = new CustomGameRuleCategory(new Identifier("alldeath", "messages"), Text.of("Death Messages"));
+
+	@Override
+	public void onInitialize() {
+		// Even though empty, it is required for fabric to initialize static variables, (and thus game rules), at the required time.
+	}
+
+	private static GameRules.Key<GameRules.BooleanRule>	BooleanRule(String name, boolean defaultValue){
+		AllDeathMessages.LOGGER.warn("Registering: {}", name);
+		return GameRuleRegistry.register("showDeathMessages"+name, Category.CHAT, GameRuleFactory.createBooleanRule(defaultValue));
+	}
+}
--- src/main/java/net/fabricmc/alldeath/mixin/Thanatos.java
+++ src/main/java/net/fabricmc/alldeath/mixin/Thanatos.java
@@ -1,10 +1,15 @@
 package net.fabricmc.alldeath.mixin;
 
 import net.fabricmc.alldeath.AllDeathMessages;
+import net.fabricmc.alldeath.DeathRules;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageTracker;
 import net.minecraft.entity.passive.TameableEntity;
-import net.minecraft.text.Text;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import net.minecraft.world.GameRules.BooleanRule;
+
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,18 +17,44 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public class Thanatos {
+public class Thanatos 
+{
+	private static boolean	HandleEntityDeath(LivingEntity entity){
+		return !entity.world.isClient
+			&& GetRule(entity.world, GameRules.SHOW_DEATH_MESSAGES)
+			&& !(entity instanceof TameableEntity && ((TameableEntity)entity).getOwner() != null)
+			;
+	}
+
+	private static boolean	HasDeathRule(LivingEntity entity){
+		return (GetRule(entity.world, DeathRules.NAMED_DEATH) && entity.hasCustomName())
+			|| (GetRule(entity.world, DeathRules.OTHER_DEATH))
+			;
+	}
+
+	private static boolean	GetRule(World world, GameRules.Key<BooleanRule> key){
+		BooleanRule rule = world.getGameRules().get(key);
+		if (rule != null)
+			return rule.get();
+		else {
+			AllDeathMessages.LOGGER.error("The rule {} doesn't exist", key.getName());
+			return false;
+		}
+	}
+
+
 	@Inject(
 		method = "onDeath(Lnet/minecraft/entity/damage/DamageSource;)V",
 		at = @At("HEAD")
 	)
 	private void InterceptDeathMessage(CallbackInfo info) {
 		LivingEntity dyingEntity = (LivingEntity)(Object)this;
-		if (dyingEntity instanceof TameableEntity && ((TameableEntity)dyingEntity).getOwner() != null)
+
+		if (!HandleEntityDeath(dyingEntity))
 			return;
 
-		Text message = dyingEntity.getDamageTracker().getDeathMessage();
-		if (!dyingEntity.world.isClient)
-			MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(message);
+		DamageTracker damages = dyingEntity.getDamageTracker();
+		if (HasDeathRule(dyingEntity))
+			MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(damages.getDeathMessage());
 	}
 }
