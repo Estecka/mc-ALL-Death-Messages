f7db7fa0ee324ce8b4b079d835912b77643de2bd
Estecka <estecka@galacsys.com>
2023-10-05 17:21:02 +0200

    Added death coordinates gamerule

--- src/main/java/tk/estecka/alldeath/mixin/EntityMixin.java
+++ src/main/java/tk/estecka/alldeath/mixin/EntityMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
+import tk.estecka.alldeath.AllDeathMessages;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin 
@@ -15,6 +16,9 @@ public abstract class EntityMixin
 	@ModifyArg( method="getHoverEvent", index=2, at=@At(value="INVOKE", target="net/minecraft/text/HoverEvent$EntityContent.<init> (Lnet/minecraft/entity/EntityType;Ljava/util/UUID;Lnet/minecraft/text/Text;)V"))
 	Text	alldeath$PosInsertion(Text entityName){
 		Entity entity = (Entity)(Object)this;
+		if (!entity.getWorld().getGameRules().getBoolean(AllDeathMessages.COORD_RULE))
+			return entityName;
+
 		MutableText text = Text.empty();
 		BlockPos pos = entity.getBlockPos();
 
