318f35dcdd07f882ac101fe4d2c546429c299c8f
Estecka <estecka@galacsys.com>
2023-10-07 23:15:17 +0200

    Fix wither and Ender Dragon being ephemeral

--- src/main/java/tk/estecka/alldeath/EntityPredicates.java
+++ src/main/java/tk/estecka/alldeath/EntityPredicates.java
@@ -4,6 +4,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.WitherEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.PassiveEntity;
@@ -16,14 +18,19 @@ public class EntityPredicates {
 		put( "hostile",    EntityPredicates::HOSTILE    );
 		put( "passive",    EntityPredicates::PASSIVE    );
 		put( "ephemeral",  EntityPredicates::EPHEMERAL  );
+		put( "semi-persistent",  EntityPredicates::SEMIPERSISTENT);
 	}};
 
 	static public boolean	NAMED(Entity e) { return e.hasCustomName() || e.isPlayer(); }
 	static public boolean	HOSTILE(Entity e) { return e instanceof HostileEntity; }
 	static public boolean	PASSIVE(Entity e) { return e instanceof PassiveEntity; }
 	static public boolean	EPHEMERAL(Entity e) { return !PERSISTENT(e); }
+	static public boolean	SEMIPERSISTENT(Entity e) { return e instanceof MobEntity mob && mob.cannotDespawn(); }
 
 	static public boolean	PERSISTENT(Entity entity) {
+		if (entity instanceof WitherEntity || entity instanceof EnderDragonEntity)
+			return true;
+
 		if (!(entity instanceof MobEntity mob))
 			return false;
 
